6d4c142768fcad21a3cb6e7ca44ebfc2845e26e4
DELL <DELL@DESKTOP-5LL6658>
2026-03-05 15:04:54 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Dictionary<int, string>estudiantes = new Dictionary<int, string>();
+			estudiantes.Add(123, "Pablo");
+			estudiantes.Add(345, "Pedro");
+			estudiantes.Add(567, "Juan");
+			Console.WriteLine(estudiantes[123]);
+
+		}
+	}
+}
