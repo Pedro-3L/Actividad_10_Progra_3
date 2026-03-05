3d604caa07188442c6e7a2eb4a9342dd22005f86
Pedro-3L <lando133@gmail.com>
2026-03-05 17:57:49 -0600

    Adicion del ejercicio2

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Dictionary<int, string> productos = new Dictionary<int, string>();
+			productos.Add(2020, "Cereal");
+			productos.Add(2010, "Pan");
+			productos.Add(2030, "Manzna ");
+
+			foreach (var item in productos)
+			{
+				Console.WriteLine(item.Key+"|"+item.Value);
+			}
+
+
+
+
+		}
+	}
+}
