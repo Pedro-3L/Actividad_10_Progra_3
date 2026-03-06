6d742aaf8205e5e8dd00abdab5d4e504550e5fa1
Pedro-3L <lando133@gmail.com>
2026-03-05 22:49:27 -0600

    Adicion del ejercicio 7

---
+++ Ejercicio7/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio7
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Dictionary<int, string> productos = new Dictionary<int, string>();
+
+			int codigo;
+			string nombre;
+
+			for (int i = 0; i < 5; i++)
+			{
+				Console.Clear();
+				Console.WriteLine("Ingrese el codigo del producto:");
+				codigo = int.Parse(Console.ReadLine());
+
+				if (productos.ContainsKey(codigo))
+				{
+					Console.WriteLine("El codigo ya existe. No se puede agregar.");
+				}
+				else
+				{
+					Console.WriteLine("Ingrese el nombre del producto:");
+					nombre = Console.ReadLine();
+					productos.Add(codigo, nombre);
+				}
+				Console.ReadKey();
+			}
+
+			Console.WriteLine("LISTA DE PRODUCTOS: ");
+
+			foreach (var producto in productos)
+			{
+				Console.WriteLine(producto.Key + " - " + producto.Value);
+			}
+		}
+	}
+}
