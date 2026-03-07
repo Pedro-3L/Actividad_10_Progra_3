54d80fb4ad3c215a47921d5c8c0bf49fff24d5a5
Pedro-3L <lando133@gmail.com>
2026-03-07 00:20:23 -0600

    adicion  ejercicio 18

---
+++ Ejercicio_18/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ejercicio_18
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Dictionary<string, string> usuarios = new Dictionary<string, string>();
+
+			string usuario;
+			string contraseña;
+			string respuesta = " ";
+			do
+			{
+				Console.WriteLine("Ingrese el nombre de usuario:");
+				usuario = Console.ReadLine();
+
+				if (usuarios.ContainsKey(usuario))
+				{
+					Console.WriteLine("El usuario ya existe.");
+				}
+				else
+				{
+					Console.WriteLine("Ingrese la contraseña:");
+					contraseña = Console.ReadLine();
+
+					usuarios.Add(usuario, contraseña);
+					Console.WriteLine("Usuario registrado correctamente.");
+				}
+				Console.WriteLine("Desea ingresar otro : ");
+				respuesta = Console.ReadLine();
+			} while (respuesta != "no" && respuesta == "si");
+
+			Console.WriteLine("LISTA DE USUSARIOS INGRESADOS:");
+
+			foreach (var item in usuarios)
+			{
+				Console.WriteLine("Usuario: " + item.Key + " Contraseña: " + item.Value);
+			}
+
+		}
+	}
+}
