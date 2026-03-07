524ba7c48c1217f6199b55fef0d01298f4a16bb2
Pedro-3L <lando133@gmail.com>
2026-03-07 13:40:59 -0600

    Adicion del ejercicio 19 y 20

---
+++ Ejercicio_19/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_19
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Dictionary<string, double> estudiantes = new Dictionary<string, double>();
+			int opcion;
+			do
+			{
+				Console.WriteLine("1 Agregar estudiante");
+				Console.WriteLine("2 Modificar");
+				Console.WriteLine("3 Mostrar");
+				Console.WriteLine("0 Salir");
+				Console.Write("Seleccione una opcion: ");
+				opcion = int.Parse(Console.ReadLine());
+				switch (opcion)
+				{
+					case 1:
+						Console.Write("Ingrese el nombre: ");
+						string nombre = Console.ReadLine();
+						Console.Write("Ingrese la nota: ");
+						double nota = double.Parse (Console.ReadLine());
+						estudiantes.Add(nombre, nota);
+						break;
+					case 2:
+						Console.Write("Ingrese el nombre del estudiante: ");
+						nombre = Console.ReadLine();
+						Console.Write("Ingrese la nueva nota: ");
+						nota = int.Parse(Console.ReadLine());
+						estudiantes[nombre] = nota;
+						break;
+					case 3:
+						foreach (var item in estudiantes)
+						{
+							Console.WriteLine("Nombre: " + item.Key + " Nota: " + item.Value);
+						}
+						break;
+				}
+			} while (opcion != 0);
+		}
+
+	}
+}
+
---
+++ Ejercicio_20/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_20
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Dictionary<int, (string	Nombre, int Edad, string Carrera)> estudiantes= new Dictionary<int, (string, int, string)>();
+
+
+			for (int i = 0; i < 3; i++)
+			{
+				Console.WriteLine("Ingrese el carné del estudiante: ");
+				int carne = int.Parse(Console.ReadLine());
+
+				Console.WriteLine("Ingrese el nombre:");
+				string nombre = Console.ReadLine();
+
+				Console.WriteLine("Ingrese la edad:");
+				int edad = int.Parse(Console.ReadLine());
+				if (edad<1 || edad>100)
+				{
+					Console.WriteLine("edad no valida");
+				}
+				else
+				{
+					Console.WriteLine("Ingrese la carrera:");
+					string carrera = Console.ReadLine();
+					estudiantes.Add(carne, (nombre, edad, carrera));
+
+				}
+			}
+
+			Console.WriteLine("LISTA DE ESTUDIANTES");
+
+			foreach (var estudiante in estudiantes)
+			{
+				Console.WriteLine("Carné: " + estudiante.Key + " Nombre: " + estudiante.Value.Nombre +  " Edad: " + estudiante.Value.Edad +	  " Carrera: " + estudiante.Value.Carrera);
+			}
+		}
+	}
+}
